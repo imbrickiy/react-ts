03cf4002a08369575097abc3b16b627bc77cea11
imbrickiy <imbrickiy@ya.ru>
2026-04-08 15:01:39 +0300

    Update dependencies and add file system capabilities
    - Added `@tauri-apps/plugin-fs` dependency to enable file system operations.
    - Updated `.gitignore` to comment out `Cargo.lock`.
    - Modified `README.md` to include project description and run instructions.
    - Implemented file writing functionality in `App.tsx`.
    - Updated `Cargo.toml` to include `tauri-plugin-fs`.
    - Adjusted permissions in `default.json` for file system access.
    - Initialized the file system plugin in `lib.rs`.

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ This template should help get you started developing with Tauri, React and Types
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+
+
+
+# Project
+
+React + Tauri app
+
+## Run
+npm install
+npm run tauri dev
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
+import { writeTextFile } from '@tauri-apps/plugin-fs';
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setGreetMsg(await invoke("greet", { name }));
+    await writeTextFile('test.txt', 'hello');
   }
 
   return (
